30542be59ccfc21da1f83a50eae8bf215c1fcc71
JoseGuevara29 <guevarabus29@gmail.com>
2021-03-11 23:49:35 -0500

    completed the MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import SavedList from './Movies/SavedList';
+import { Route } from "react-router-dom";
 
-export default function App () {
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+        .then((response) => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data);
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
+      {/* render = {() => <MovieList movie={movieList}/> Used for passing in the component and the props. */}
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" render={() => <MovieList movies={movieList} />} />
+      <Route
+        exact
+        path="/movies/:id"
+        render={() => <Movie movies={movieList} />}
+      />
     </div>
   );
 }
